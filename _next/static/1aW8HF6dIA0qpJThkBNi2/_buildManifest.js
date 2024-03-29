750014a62d03ffac0e353013cb5f2ecf0460f3e4
mtw <382160702@qq.com>
2024-03-29 16:33:39 +0800

    deploy

---
+++ _next/static/1aW8HF6dIA0qpJThkBNi2/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-ec471d67e97d09e2.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
